85858ef0da71a07dc0b6179d78381911b8297e99
Kayan <1443700673@qq.com>
2022-05-03 15:04:30 +0800

    前端实现针对学生表的CRUD功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -95,37 +95,102 @@
 
       <el-main>
         <div style="margin: 0 0 10px 0">
-          <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search"></el-input><el-button style="margin-left: 5px" type="primary">搜索</el-button>
+          <el-input style="width: 200px" placeholder="请输入学号" suffix-icon="el-icon-search" v-model="id"></el-input>
+          <el-input style="width: 200px; margin-left: 5px" placeholder="请输入姓名" suffix-icon="el-icon-user" v-model="name" ></el-input>
+          <el-input style="width: 200px; margin-left: 5px" placeholder="请输入专业" suffix-icon="el-icon-notebook-2" v-model="major"></el-input>
+          <el-input style="width: 200px; margin-left: 5px" placeholder="请输入班级" suffix-icon="el-icon-s-home" v-model="clas"></el-input>
+          <el-button style="margin-left: 5px" type="primary" @click="load">搜索</el-button>
+          <el-button style="margin-left: 5px" type="warning" @click="reset">重置</el-button>
         </div>
         <div style="margin: 10px 0">
-          <el-button type="primary" size="medium">新增<i class="el-icon-circle-plus-outline"></i></el-button>
-          <el-button type="danger" size="medium">批量删除<i class="el-icon-remove-outline"></i></el-button>
-          <el-button type="primary" size="medium">导入<i class="el-icon-bottom"></i></el-button>
-          <el-button type="primary" size="medium">导出<i class="el-icon-top"></i></el-button>
+          <el-button type="primary" size="medium" @click="handleAdd">新增<i class="el-icon-circle-plus-outline"></i></el-button>
+          <el-popconfirm
+              style="margin-left: 5px"
+              confirm-button-text='确定'
+              cancel-button-text='我再想想'
+              icon="el-icon-info"
+              icon-color="red"
+              title="您确定批量删除这些数据吗？"
+              @confirm = "delBatch"
+          >
+            <el-button type="danger" size="medium" slot="reference">批量删除<i class="el-icon-remove-outline"></i></el-button>
+          </el-popconfirm>
+          <el-button type="primary" size="medium" style="margin-left: 5px">导入<i class="el-icon-bottom"></i></el-button>
+          <el-button type="primary" size="medium" style="margin-left: 5px">导出<i class="el-icon-top"></i></el-button>
         </div>
-        <el-table :data="tableData" border stripe header-cell-class-name="headerBg">
-          <el-table-column prop="date" label="日期" width="140">
+        <el-table :data="tableData" border stripe header-cell-class-name="headerBg" @selection-change="handleSelectionChange">
+          <el-table-column type="selection" width="55"></el-table-column>
+          <el-table-column prop="id" label="学号" width="140">
           </el-table-column>
           <el-table-column prop="name" label="姓名" width="120">
           </el-table-column>
-          <el-table-column prop="address" label="地址">
+          <el-table-column prop="major" label="专业">
+          </el-table-column>
+          <el-table-column prop="clas" label="班级">
+          </el-table-column>
+          <el-table-column prop="phone" label="电话">
+          </el-table-column>
+          <el-table-column prop="email" label="邮箱">
           </el-table-column>
           <el-table-column label="操作">
             <template slot-scope="scope">
-              <el-button type="success" size="mini">编辑<i class="el-icon-edit"></i></el-button>
-              <el-button type="danger" size="mini">删除<i class="el-icon-remove-outline"></i></el-button>
+              <el-button type="success" size="mini" @click="handleEdit(scope.row)">编辑<i class="el-icon-edit"></i></el-button>
+              <el-popconfirm
+                  style="margin-left: 5px"
+                  confirm-button-text='确定'
+                  cancel-button-text='我再想想'
+                  icon="el-icon-info"
+                  icon-color="red"
+                  title="您确定删除吗？"
+                  @confirm = "handleDelete(scope.row.id)"
+              >
+                <el-button type="danger" size="mini" slot="reference">删除<i class="el-icon-remove-outline"></i></el-button>
+              </el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
         <div style="padding: 10px 0">
           <el-pagination
-
-                  :page-sizes="[5, 10, 15, 20]"
-                  :page-size="10"
-                  layout="total, sizes, prev, pager, next, jumper"
-                  :total="400">
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="pageNum"
+              :page-sizes="[5, 10, 20, 50]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="total">
           </el-pagination>
         </div>
+
+        <el-dialog title="学生信息" :visible.sync="dialogFormVisible" width="30%">
+          <el-form label-width="80px" size="small">
+            <el-form-item label="学号">
+              <el-input v-model="form.id" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input v-model="form.password" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="姓名">
+              <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="专业">
+              <el-input v-model="form.major" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="班级">
+              <el-input v-model="form.clas" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <el-input v-model="form.email" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="电话">
+              <el-input v-model="form.phone" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="save">确 定</el-button>
+          </div>
+        </el-dialog>
+
       </el-main>
     </el-container>
   </el-container>
@@ -133,21 +198,33 @@
 
 <script>
 
+import request from "@/utils/request";
+
 export default {
   name: 'HomeView',
   data() {
-    const item = {
-      date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    };
     return {
-      tableData: Array(10).fill(item),
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 10,
+      id: "",
+      name: "",
+      major: "",
+      clas: "",
+
+      dialogFormVisible: false,
+      form: {},
+      multipleSelection: [],
+
       collapseBtnClass: 'el-icon-s-fold',
       isCollapse: false,
       sideWidth: 200,
       logoTextShow: true
     }
+  },
+  created() {
+    this.load()
   },
   methods: {
     collapse() {  //点击收缩按钮触发
@@ -161,6 +238,81 @@ export default {
         this.collapseBtnClass = 'el-icon-s-fold'
         this.logoTextShow = true
       }
+    },
+    load() {
+      request.get("/student/page",{
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          id: this.id,
+          name: this.name,
+          major: this.major,
+          clas: this.clas
+        }
+      }).then(res => {
+        console.log(res)
+        this.tableData = res.records
+        this.total = res.total
+      })
+
+    },
+    save() {
+      request.post("/student",this.form).then(res => {
+        if(res){
+          this.$message.success("保存成功")
+          this.dialogFormVisible = false
+          this.load()
+        } else {
+          this.$message.error("保存失败")
+        }
+      })
+    },
+    reset() {
+      this.id= ""
+      this.name= ""
+      this.major= ""
+      this.clas= ""
+      this.load()
+    },
+    handleAdd() {
+      this.dialogFormVisible = true
+      this.form = {}
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    delBatch() {
+      let ids = this.multipleSelection.map(v => v.id) //把对象数组转换为纯id的数组
+      request.post("/student/del/batch", ids).then(res => {
+        if(res){
+          this.$message.success("批量删除成功")
+          this.load()
+        } else {
+          this.$message.error("批量删除失败")
+        }
+      })
+    },
+    handleEdit(row) {
+      this.form = Object.assign({},row)
+      this.dialogFormVisible = true
+    },
+    handleDelete(id) {
+      request.delete("/student/" + id).then(res => {
+        if(res){
+          this.$message.success("删除成功")
+          this.load()
+        } else {
+          this.$message.error("删除失败")
+        }
+      })
+    },
+    handleSizeChange(pageSize) {
+      this.pageSize = pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum) {
+      this.pageNum = pageNum
+      this.load()
     }
   }
 }
